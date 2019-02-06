5c2f666080d4b3d4e7498a90569fdc68d8c4fb90
Saksham <sakshammanocha14@gmail.com>
2019-02-06 21:58:57 +0530

    Changed Icon and geolocation

--- public/src/index.js
+++ public/src/index.js
@@ -12,6 +12,16 @@ socket.on('disconnect',()=>{
   console.log('User is Disconnected');
 })
 
+socket.on('newLocationMessage', function (message) {
+  var li = $('<li></li>');
+  var a = $('<a target="_blank">My current location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  $('#messages').append(li);
+});
+
 socket.on('newMessage', function (message) {
   var li = $('<li></li>');
   li.text(`${message.from}: ${message.text}`);
@@ -26,5 +36,21 @@ socket.emit('createMessage', {
     from: 'User',
     text: $(".message").val()
   }, function () {
+  });
+});
+
+var locationButton = $('#send-location');
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    alert('Unable to fetch location.');
   });
 });
