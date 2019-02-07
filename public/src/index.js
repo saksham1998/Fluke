bd9403ad90f98ec3e44385909ee4568376e7ec69
Saksham <sakshammanocha14@gmail.com>
2019-02-07 13:19:22 +0530

    .

--- public/src/index.js
+++ public/src/index.js
@@ -45,13 +45,16 @@ locationButton.on('click', function () {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser.');
   }
+  locationButton.attr('disabled','disabled').text('Sending Location......');
 
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send Location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    locationButton.removeAttr('disabled').text('Send Location');
     alert('Unable to fetch location.');
   });
 });
